14234d38b98902c5e140e12af560d4bf5dd772dc
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-03-31 19:55:01 +0200

    Updated the Dockerfile

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,12 +3,19 @@ package hello;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         String trimmed = StringUtils.trim("   hejsan    ");
 
-        System.out.println(trimmed);
+        System.out.print("Write your name: ");
+        var name = sc.nextLine();
+
+        System.out.println(trimmed + name);
 
     }
 }
